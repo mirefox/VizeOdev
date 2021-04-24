e5cc0ff8d49217442bf0b7076fbb3a82a0c685e3
mirefox <tahsincan07@gmail.com>
2021-04-25 01:45:08 +0300

    ilkAdim

--- VizeOdev/Form1.Designer.cs
+++ VizeOdev/Form1.Designer.cs
@@ -29,13 +29,47 @@ namespace VizeOdev
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.haberBaslikları = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(539, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // haberBaslikları
+            // 
+            this.haberBaslikları.FormattingEnabled = true;
+            this.haberBaslikları.ItemHeight = 16;
+            this.haberBaslikları.Location = new System.Drawing.Point(12, 12);
+            this.haberBaslikları.Name = "haberBaslikları";
+            this.haberBaslikları.Size = new System.Drawing.Size(481, 420);
+            this.haberBaslikları.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1095, 450);
+            this.Controls.Add(this.haberBaslikları);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox haberBaslikları;
     }
 }
 
--- VizeOdev/Form1.cs
+++ VizeOdev/Form1.cs
@@ -16,5 +16,10 @@ namespace VizeOdev
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("deneme");
+        }
     }
 }
